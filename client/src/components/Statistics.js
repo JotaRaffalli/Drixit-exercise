55c25da6933ffe7b28f89fe91cb5f4e0c29ee8db
José Luis Raffalli <raffallijoseluis@gmail.com>
2019-08-14 23:56:35 -0300

    Update Statistics.js
    Created component.
    Fetch Data exposed by Backend

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class Statistics extends React.Component {
- render() {
-     return (
-         <h1>Hola Mundo</h1>
-     )
- }
+    constructor() {
+        super();
+        this.state = {
+            Statistics: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/statistics')
+        .then(res => res.json())
+        .then(statistics => this.setState({statistics}, () => console.log('Here are the statistics: ',statistics)))
+        .catch(err => console.log('There was an error getting data from API'))
+    }
+
+    render() {
+        return (
+            <h1>Hola Mundo</h1>
+        )
+    }
 }
 
 export default Statistics;
