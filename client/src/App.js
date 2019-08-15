1d6dbd13173e8cf8c1e8e28a8cd14438f3d7e1e1
José Luis Raffalli <raffallijoseluis@gmail.com>
2019-08-14 23:40:36 -0300

    Client Created
    Client created using Create React App.
    Statistics componet created

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Statitics from './components/Statistics';
+function App() {
+  return (
+    <div className="App">
+      <Statitics></Statitics>
+    </div>
+  );
+}
+
+export default App;
